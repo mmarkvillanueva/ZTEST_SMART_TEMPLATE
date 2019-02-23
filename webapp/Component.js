3d54a376bc8f793b03dfc53498a52b853b6a1d09
mmarkvillanueva <mmarkvillanueva@gmail.com>
2019-02-24 02:28:31 +0800

    Initial Creation of List Report Template from Original ODATA Service Using SAP Annotation

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.dxc.testztest_smart_template.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.dxc.testztest_smart_template.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
